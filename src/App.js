6cb5bc6b4359123d8a5de37822d2f28758d21a00
jiggly-jo <huynhjohn21@yahoo.com>
2023-06-13 15:42:55 -0600

    HTTP Requests and updating state in react

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
 import React, {Component} from 'react';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component{
+  state = {
+    users: [],
+    loading: false
+  }
+  // life cycle method runs when component mounts or when component loads
+  async componentDidMount() {
+    //this is how you set the state in react
+    this.setState({loading: true});
+    const res =  await axios.get('https://api.github.com/users');
+    this.setState({users: res.data, loading: false});
+    
+  }
   render(){
 
     return (
       <div>
         <Navbar />
         <div className='container'>
-          <Users />
+          <Users loading={this.state.loading} users={this.state.users}/>
         </div>
         
       </div>
